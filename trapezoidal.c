183b75275364e70bfd2358c464d1044cebc77f78
Hari Nandan Nath <hari672@gmail.com>
2024-04-13 16:30:29 +0545

    Added trapezoidal.c

---
+++ trapezoidal.c
@@ -0,0 +1,25 @@
+/* Trapezoidal Rule */
+#include <stdio.h>
+#include <math.h>
+float f(float x)
+{
+    return pow(x, 2);
+}
+int main()
+{
+    int n = 10;
+    float a = 1, b = 3, intg;
+    float sum = f(a) + f(b);
+    float x0 = a;
+    float h = (b - a) / n;
+    int i;
+    for (i = 1; i <= n - 1; i++)
+    {
+        x0 += h;
+        sum += 2 * f(x0);
+    }
+    intg = h / 2.0 * sum;
+    printf("The integral is %g\n", intg);
+
+    return 0;
+}
