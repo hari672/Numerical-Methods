08fde0feff29c9536a53c034b66e47ab75453ab1
Hari Nandan Nath <hari672@gmail.com>
2024-04-13 16:04:30 +0545

    diffTable.c added

--- diffTable.c
+++ diffTable.c
@@ -1,12 +1,12 @@
 /*
 Derivative of tabulated functions using forward difference formula
 x: 1    1.1     1.2     1.3
-y: 1    1.21    1.44    1.69 
+y: 1    1.21    1.44    1.69
 Find f'(1)
 */
 
-#include<stdio.h>
-#include<math.h>
+#include <stdio.h>
+#include <math.h>
 
 #define n 4
 
@@ -15,10 +15,18 @@ int main()
     float x[n] = {1, 1.1, 1.2, 1.3};
     float y[n] = {1, 1.21, 1.44, 1.69};
     float h = x[1] - x[0];
-    int i;
-    for(i = 1; i < n; i++)
+    int i, j;
+    float sum = 0, coeff, der;
+    for (i = 0; i < n - 1; i++)
     {
-        y[i] = y[i + 1] - y[i];
+        for (j = 0; j < n - 1 - i; j++)
+        {
+            y[j] = y[j + 1] - y[j];
+        }
+        coeff = pow(-1, i) / (i + 1);
+        sum += coeff * y[0];
     }
+    der = 1 / h * sum;
+    printf("f'(%g) = %g\n", x[0], der);
     return 0;
 }
